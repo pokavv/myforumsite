0dc6fb8308c08dd5e5b5a7dab4c406f30818941c
pokavv <okaps@hanmail.net>
2023-03-28 02:42:08 +0900

    230328 updated

--- board/models.py
+++ board/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    postname = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
     post_image = models.ImageField(blank=True, null=True)
     contents = models.TextField()
+    dt_created = models.DateTimeField(verbose_name='작성일', auto_now_add=True)
+    dt_modified = models.DateTimeField(verbose_name='수정일', auto_now=True)
     
     # postname을 Post Object 대신 나오게함
     def __str__(self):
