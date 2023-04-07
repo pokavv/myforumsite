6db66f0fe9caa16755b1de8bd2fcaa80a09b13e4
pokavv <okaps@hanmail.net>
2023-04-07 18:39:43 +0900

    230407 updated

--- board/forms.py
+++ board/forms.py
@@ -5,18 +5,17 @@ from django.core.exceptions import ValidationError
 
 class PostForm(forms.ModelForm):
     
-    class Meta: # PostForm class에 적용할 옵션을 작성하는 class
-        models = Post
+    class Meta: # Postform class 에 적용할 옵션 작성하는 class
+        model = Post
         fields = ['title', 'contents']
         widgets = {'title': forms.TextInput(attrs={
                         'class':'title',
                         'placeholder':'제목을 입력하세요.'}),
                     'contents':forms.Textarea(attrs={
-                        'placeholder':'내용을 입력하세요.'})
-                    }
+                        'placeholder':'내용을 입력하세요.'})}
     
     def clean_title(self):
         title = self.cleaned_data['title']
         if '*' in title:
-            raise ValidationError('* 는 포함될 수 없습니다.')
+            raise ValidationError('*는 포함될 수 없습니다.')
         return title
